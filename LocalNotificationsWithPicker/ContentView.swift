8a221a6cd0c3f2f8ae70012ed16e32ec2daef9eb
João Victor Ferreira Pimenta <joaovictorfp30@gmail.com>
2022-09-05 11:33:41 -0300

    Initial Commit

---
+++ LocalNotificationsWithPicker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LocalNotificationsWithPicker
+//
+//  Created by João Victor Ferreira Pimenta on 05/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LocalNotificationsWithPicker/LocalNotificationsWithPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalNotificationsWithPickerApp.swift
+//  LocalNotificationsWithPicker
+//
+//  Created by João Victor Ferreira Pimenta on 05/09/22.
+//
+
+import SwiftUI
+
+@main
+struct LocalNotificationsWithPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
